dbf33fe66fc111cc25575b7e8688f228cab9ab04
Bertrand Alain Gregor <alain.bertrand@psi.ch>
2017-09-19 13:35:33 +0000

    git-tfs-id: [https://tfs.psi.ch/DefaultCollection/]$/Epics Gateway;C2119

--- GW3/GatewayLogic/Services/Log.cs
+++ GW3/GatewayLogic/Services/Log.cs
@@ -16,17 +16,32 @@ namespace GatewayLogic.Services
 
     internal class Log
     {
+        public delegate void LogHandler(string source, string message);
+        public delegate bool LogFilter(LogLevel level);
+
+        public event LogHandler Handler = Log.DefaultHandler;
+        public event LogFilter Filter = Log.ShowAll;
+
+        private static bool ShowAll(LogLevel level)
+        {
+            return true;
+        }
+
+        private static void DefaultHandler(string source, string message)
+        {
+            Console.Write(DateTime.Now.ToString("HH:mm:ss"));
+            Console.Write(" - ");
+            Console.Write(source);
+            Console.Write("\t");
+            Console.WriteLine(message);
+        }
 
         public void Write(LogLevel level, string message,
             [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
             [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
             [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
         {
-            Console.Write(DateTime.Now.ToString("HH:mm:ss"));
-            Console.Write(" - ");
-            Console.Write(sourceFilePath.Split(new char[] { '\\' }).Last().Split(new char[] { '.' }).First() + "." + memberName + ":" + sourceLineNumber);
-            Console.Write("\t");
-            Console.WriteLine(message);
+            Handler?.Invoke(sourceFilePath.Split(new char[] { '\\' }).Last().Split(new char[] { '.' }).First() + "." + memberName + ":" + sourceLineNumber, message);
         }
     }
 }
