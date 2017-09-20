2021214d094eb4e9adce801067b07f1dbccc1cfe
Bertrand Alain Gregor <alain.bertrand@psi.ch>
2017-09-20 09:13:30 +0000

    git-tfs-id: [https://tfs.psi.ch/DefaultCollection/]$/Epics Gateway;C2122

--- GW3/GatewayLogic/Commands/EventAdd.cs
+++ GW3/GatewayLogic/Commands/EventAdd.cs
@@ -11,69 +11,73 @@ namespace GatewayLogic.Commands
 {
     class EventAdd : CommandHandler
     {
-        SemaphoreSlim lockObject = new SemaphoreSlim(1);
+        object lockObject = new object();
 
         public override void DoRequest(GatewayConnection connection, DataPacket packet)
         {
-            lockObject.Wait();
-            var channel = connection.Gateway.ChannelInformation.Get(packet.Parameter1);
-            if (channel == null)
+            lock (lockObject)
             {
-                connection.Gateway.Log.Write(Services.LogLevel.Error, "Event add on wrong channel.");
-                lockObject.Release();
-                return;
+                var channel = connection.Gateway.ChannelInformation.Get(packet.Parameter1);
+                if (channel == null)
+                {
+                    connection.Gateway.Log.Write(Services.LogLevel.Error, "Event add on wrong channel.");
+                    return;
+                }
+                connection.Gateway.Log.Write(Services.LogLevel.Detail, "Event add on " + channel.ChannelName);
+
+                // A monitor on datacount 0 will always be a new monitor
+                var monitor = connection.Gateway.MonitorInformation.Get(channel, packet.DataType, packet.DataCount);
+                // A fresh new monitor
+                if (monitor.FirstTime == true)
+                {
+                    connection.Gateway.Log.Write(Services.LogLevel.Detail, "First event");
+                    monitor.FirstTime = false;
+
+                    monitor.AddClient(new ClientId { Client = packet.Sender, Id = packet.Parameter2 });
+                    var newPacket = (DataPacket)packet.Clone();
+                    newPacket.Parameter1 = channel.ServerId.Value;
+                    newPacket.Parameter2 = monitor.GatewayId;
+                    newPacket.Destination = channel.TcpConnection.RemoteEndPoint;
+                    channel.TcpConnection.Send(newPacket);
+                }
+                // We must send a Read Notify to get the first result
+                else if (monitor.HasReceivedFirstResult == true)
+                {
+                    monitor.AddClient(new ClientId { Client = packet.Sender, Id = packet.Parameter2, WaitingReadyNotify = true });
+
+                    connection.Gateway.Log.Write(Services.LogLevel.Detail, "First event result already sent. Sent ReadNotify.");
+
+                    var read = connection.Gateway.ReadNotifyInformation.Get(channel, packet.Parameter2, (TcpClientConnection)connection);
+                    read.IsEventAdd = true;
+                    read.EventClientId = packet.Parameter2;
+                    read.Monitor = monitor;
+
+                    var newPacket = DataPacket.Create(0);
+                    newPacket.Command = 15;
+                    newPacket.DataType = packet.DataType;
+                    newPacket.DataCount = packet.DataCount;
+                    newPacket.Parameter1 = channel.ServerId.Value;
+                    newPacket.Parameter2 = read.GatewayId;
+                    channel.TcpConnection.Send(newPacket);
+                }
+                else
+                {
+                    connection.Gateway.Log.Write(Services.LogLevel.Detail, "Add client to the waiting list.");
+                    monitor.AddClient(new ClientId { Client = packet.Sender, Id = packet.Parameter2 });
+                }
             }
-            connection.Gateway.Log.Write(Services.LogLevel.Detail, "Event add on " + channel.ChannelName);
-
-            var monitor = connection.Gateway.MonitorInformation.Get(channel, packet.DataType, packet.DataCount);
-            if (monitor.FirstTime == true)
-            {
-                connection.Gateway.Log.Write(Services.LogLevel.Detail, "First event");
-                monitor.FirstTime = false;
-
-                monitor.AddClient(new ClientId { Client = packet.Sender, Id = packet.Parameter2 });
-                var newPacket = (DataPacket)packet.Clone();
-                newPacket.Parameter1 = channel.ServerId.Value;
-                newPacket.Parameter2 = monitor.GatewayId;
-                newPacket.Destination = channel.TcpConnection.RemoteEndPoint;
-                channel.TcpConnection.Send(newPacket);
-            }
-            // We must send a Read Notify to get the first result
-            else if (monitor.HasReceivedFirstResult == true)
-            {
-                monitor.AddClient(new ClientId { Client = packet.Sender, Id = packet.Parameter2, WaitingReadyNotify = true });
-
-                connection.Gateway.Log.Write(Services.LogLevel.Detail, "First event result already sent. Sent ReadNotify.");
-
-                var read = connection.Gateway.ReadNotifyInformation.Get(channel, packet.Parameter2, (TcpClientConnection)connection);
-                read.IsEventAdd = true;
-                read.EventClientId = packet.Parameter2;
-                read.Monitor = monitor;
-
-                var newPacket = DataPacket.Create(0);
-                newPacket.Command = 15;
-                newPacket.DataType = packet.DataType;
-                newPacket.DataCount = packet.DataCount;
-                newPacket.Parameter1 = channel.ServerId.Value;
-                newPacket.Parameter2 = read.GatewayId;
-                channel.TcpConnection.Send(newPacket);
-            }
-            else
-            {
-                connection.Gateway.Log.Write(Services.LogLevel.Detail, "Add client to the waiting list.");
-                monitor.AddClient(new ClientId { Client = packet.Sender, Id = packet.Parameter2 });
-            }
-            lockObject.Release();
         }
 
         public override void DoResponse(GatewayConnection connection, DataPacket packet)
         {
-            lockObject.Wait();
-            var monitor = connection.Gateway.MonitorInformation.GetByGatewayId(packet.Parameter2);
-            monitor.HasReceivedFirstResult = true;
-            connection.Gateway.Log.Write(Services.LogLevel.Detail, "Event add response on " + monitor.ChannelInformation.ChannelName);
-            var clients = monitor.GetClients();
-            lockObject.Release();
+            IEnumerable<ClientId> clients = null;
+            lock (lockObject)
+            {
+                var monitor = connection.Gateway.MonitorInformation.GetByGatewayId(packet.Parameter2);
+                monitor.HasReceivedFirstResult = true;
+                connection.Gateway.Log.Write(Services.LogLevel.Detail, "Event add response on " + monitor.ChannelInformation.ChannelName);
+                clients = monitor.GetClients();
+            }
             foreach (var client in clients)
             {
                 if (client.WaitingReadyNotify)
