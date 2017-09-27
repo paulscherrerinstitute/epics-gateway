c5d66cfcd7653e86400a16bdea275067c1871bfe
Bertrand Alain Gregor <alain.bertrand@psi.ch>
2017-09-27 07:16:45 +0000

    git-tfs-id: [https://tfs.psi.ch/DefaultCollection/]$/Epics Gateway;C2142

---
+++ GW3/GatewayLogic/Configuration/HostFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace  GatewayLogic.Configuration
+{
+    [Serializable]
+    public class HostFilter : SecurityFilter
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        public override bool Applies(string username, string hostname, string ip)
+        {
+            if (hostname == null)
+                return false;
+            return String.Compare(hostname, Name, StringComparison.OrdinalIgnoreCase) == 0;
+        }
+
+        public override void Init()
+        {
+        }
+    }
+}
