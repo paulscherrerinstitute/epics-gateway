6e88635f5f0a1f7b7c6a4da1ab7b82134bd1d4e0
Bertrand Alain Gregor <alain.bertrand@psi.ch>
2017-11-03 12:50:19 +0000

    git-tfs-id: [https://tfs.psi.ch/DefaultCollection/]$/Epics Gateway;C2251

---
+++ GW3/GatewayDebugData/SearchStat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GatewayDebugData
+{
+    public class SearchStat
+    {
+        public string Name { get;  set; }
+
+        public int NumberOfSearches { get;  set; }
+    }
+}
