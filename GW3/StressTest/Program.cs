2c9dc7dddcfdb8b2dfb1add5c4c37b6dbda91864
Bertrand Alain Gregor <alain.bertrand@psi.ch>
2017-11-06 12:48:43 +0000

    git-tfs-id: [https://tfs.psi.ch/DefaultCollection/]$/Epics Gateway;C2260

--- GW3/StressTest/Program.cs
+++ GW3/StressTest/Program.cs
@@ -4,6 +4,7 @@ using EpicsSharp.ChannelAccess.Server.RecordTypes;
 using GatewayLogic;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -14,7 +15,6 @@ namespace StressTest
 {
     class Program
     {
-
         const int NB_SERVERS = 20;
         const int NB_CLIENTS = 20;
         const int NB_CHANNELS = 10;
@@ -23,14 +23,16 @@ namespace StressTest
 
         static void Main(string[] args)
         {
-            if (args[1] == "server")
+            if (args.Length > 0 && args[0] == "server")
             {
-                Server(int.Parse(args[2]));
+                Console.WriteLine("Start server " + args[1]);
+                Server(int.Parse(args[1]));
                 return;
             }
 
-            if (args[1] == "client")
+            if (args.Length > 0 && args[0] == "client")
             {
+                Console.WriteLine("Start client " + args[1]);
                 Client();
                 return;
             }
@@ -38,19 +40,156 @@ namespace StressTest
             using (var gateway = new Gateway())
             {
                 gateway.Configuration.SideA = "127.0.0.1:5432";
-                gateway.Configuration.RemoteSideB = "";
+                var addrs = "";
+                for (var i = 0; i < NB_SERVERS; i++)
+                    addrs += (i != 0 ? ";" : "") + "127.0.0.1:" + (5056 + i);
+                gateway.Configuration.RemoteSideB = addrs;
                 gateway.Configuration.SideB = "127.0.0.1:5055";
                 gateway.Log.Filter = (level) => { return level >= GatewayLogic.Services.LogLevel.Error; };
                 gateway.Start();
 
-                for (var i = 0; i < NB_SERVERS; i++)
-                    gateway.Configuration.RemoteSideB = (i != 0 ? ";" : "") + "127.0.0.1:" + (5056 + i);
+                var current = Process.GetCurrentProcess();
+
+                var servers = Enumerable.Range(0, NB_SERVERS)
+                    .Select(i =>
+                    {
+                        var p = new Process();
+                        p.StartInfo = new ProcessStartInfo(current.ProcessName, "server " + i)
+                        {
+                            /*RedirectStandardOutput = true,
+                            RedirectStandardError = true,*/
+                            UseShellExecute = false
+                        };
+                        p.EnableRaisingEvents = true;
+                        p.Start();
+                        return p;
+                    }).ToList();
+
+                var clients = Enumerable.Range(0, NB_CLIENTS)
+                    .Select(i =>
+                    {
+                        var p = new Process();
+                        p.StartInfo = new ProcessStartInfo(current.ProcessName, "client " + i)
+                        {
+                            /*RedirectStandardOutput = true,
+                            RedirectStandardError = true,*/
+                            UseShellExecute = false
+                        };
+                        p.EnableRaisingEvents = true;
+                        p.Start();
+                        return p;
+                    }).ToList();
+
+                EventHandler serverExit = (obj, evt) => { ((Process)obj).Start(); };
+                EventHandler clientExit = (obj, evt) => { ((Process)obj).Start(); };
+                servers.ForEach(row => row.Exited += serverExit);
+                clients.ForEach(row => row.Exited += clientExit);
+
+
+
+                Console.WriteLine("Press any key to stop...");
+                Console.ReadKey();
+
+                AppDomain.CurrentDomain.ProcessExit += (obj, evt) =>
+                 {
+                     servers.ForEach(row => row.Exited -= serverExit);
+                     clients.ForEach(row => row.Exited -= clientExit);
+
+                     servers.ForEach(row => row.Kill());
+                     clients.ForEach(row => row.Kill());
+                 };
             }
         }
 
         private static void Client()
         {
+            var rnd = new Random();
+            switch (rnd.Next(0, 2))
+            {
+                case 0:
+                    Console.WriteLine("Ten sec monitor");
+                    MonitorTenSecAction();
+                    break;
+                case 1:
+                    Console.WriteLine("All monitors");
+                    MonitorOnceAction();
+                    break;
+                case 2:
+                    Console.WriteLine("Get all");
+                    GetOnceAction();
+                    break;
+            }
+        }
 
+        static void MonitorTenSecAction()
+        {
+            var rnd = new Random();
+            var channelNames = Enumerable.Range(0, NB_SERVERS * NB_CHANNELS)
+                .Select(row => new KeyValuePair<string, double>("STRESS-TEST-" + (row + 1), rnd.Next()))
+                .OrderBy(row => row.Value)
+                .Select(row => row.Key)
+                .Take(NB_CHECKED)
+                .ToList();
+
+            for (var i = 0; i < NB_LOOPS; i++)
+            {
+                using (var client = new CAClient())
+                {
+                    client.Configuration.SearchAddress = "127.0.0.1:5432";
+                    client.Configuration.WaitTimeout = 1000;
+
+                    var channels = channelNames.Select(row => client.CreateChannel<string>(row)).ToList();
+
+                    var multiEvt = new CountdownEvent(channels.Count());
+
+                    channels.ForEach((row) =>
+                    {
+                        row.MonitorChanged += (chan, val) =>
+                        {
+                            multiEvt.Signal();
+                        };
+                    });
+                    multiEvt.Wait();
+                    Thread.Sleep(10000);
+                }
+            }
+        }
+
+        static void MonitorOnceAction()
+        {
+            var rnd = new Random();
+            var channelNames = Enumerable.Range(0, NB_SERVERS * NB_CHANNELS)
+                .Select(row => new KeyValuePair<string, double>("STRESS-TEST-" + (row + 1), rnd.Next()))
+                .OrderBy(row => row.Value)
+                .Select(row => row.Key)
+                .Take(NB_CHECKED)
+                .ToList();
+
+            for (var i = 0; i < NB_LOOPS; i++)
+            {
+                using (var client = new CAClient())
+                {
+                    client.Configuration.SearchAddress = "127.0.0.1:5432";
+                    client.Configuration.WaitTimeout = 1000;
+
+                    var channels = channelNames.Select(row => client.CreateChannel<string>(row)).ToList();
+
+                    var multiEvt = new CountdownEvent(channels.Count());
+
+                    channels.ForEach((row) =>
+                    {
+                        row.MonitorChanged += (chan, val) =>
+                            {
+                                multiEvt.Signal();
+                            };
+                    });
+                    multiEvt.Wait();
+                }
+            }
+        }
+
+        static void GetOnceAction()
+        {
             var rnd = new Random();
             var channelNames = Enumerable.Range(0, NB_SERVERS * NB_CHANNELS)
                 .Select(row => new KeyValuePair<string, double>("STRESS-TEST-" + (row + 1), rnd.Next()))
@@ -84,6 +223,11 @@ namespace StressTest
                     {
                         var serverChannel = server.CreateRecord<CAStringRecord>("STRESS-TEST-" + ((i + 1) + (port - 5056 * NB_SERVERS)));
                         serverChannel.Value = "Works fine! - " + i;
+                        serverChannel.PrepareRecord += (rec, obj) =>
+                          {
+                              serverChannel.Value = "Works fine! - " + i + " - " + DateTime.UtcNow.ToLongTimeString();
+                          };
+                        serverChannel.Scan = EpicsSharp.ChannelAccess.Constants.ScanAlgorithm.HZ10;
                         serverChannels.Add(serverChannel);
                     }
                     server.Start();
