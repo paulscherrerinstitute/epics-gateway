8b56d9e72b40ca5eb339ea6c15b1593239af1c98
Bertrand Alain Gregor <alain.bertrand@psi.ch>
2017-11-10 09:43:44 +0000

    git-tfs-id: [https://tfs.psi.ch/DefaultCollection/]$/Epics Gateway;C2284

---
+++ GW3/GWWatchdog/WatchdogService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GWWatchdog
+{
+    public partial class WatchdogService : ServiceBase
+    {
+        public WatchdogService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
