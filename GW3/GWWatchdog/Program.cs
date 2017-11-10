6eb690f2710ca9fe940155fdc6e7aa7f42cd9cfc
Bertrand Alain Gregor <alain.bertrand@psi.ch>
2017-11-10 13:55:29 +0000

    git-tfs-id: [https://tfs.psi.ch/DefaultCollection/]$/Epics Gateway;C2285

--- GW3/GWWatchdog/Program.cs
+++ GW3/GWWatchdog/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,32 @@ namespace GWWatchdog
 {
     static class Program
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool AllocConsole();
+
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         static void Main()
         {
-            ServiceBase[] ServicesToRun;
+            /*ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
                 new WatchdogService()
             };
-            ServiceBase.Run(ServicesToRun);
+            ServiceBase.Run(ServicesToRun);*/
+
+            if (Environment.UserInteractive)
+            {
+                AllocConsole();
+                WatchdogService w = new WatchdogService();
+                w.Start();
+            }
+            else
+            {
+                ServiceBase.Run(new ServiceBase[] { new WatchdogService() });
+            }
         }
     }
 }
