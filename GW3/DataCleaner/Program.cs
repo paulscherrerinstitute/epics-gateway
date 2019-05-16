29f18e60c4bc0bfbf29d4cb01c7890c29514048d
bertrand <alain.bertrand@psi.ch>
2019-05-16 14:54:33 +0200

    Added support for anomalies cleanup

--- GW3/DataCleaner/Program.cs
+++ GW3/DataCleaner/Program.cs
@@ -14,7 +14,7 @@ namespace DataCleaner
     {
         static void Main(string[] args)
         {
-            if(args.Length < 2)
+            if (args.Length < 2)
             {
                 Console.WriteLine("DataCleaner [path] [days]");
                 return;
@@ -22,14 +22,32 @@ namespace DataCleaner
 
             var path = args[0];
             var days = int.Parse(args[1]);
+            var allowXML = (args.Any(row => row == "+xml"));
 
             var toTrashDate = DateTime.UtcNow.AddDays(-days);
             var toTrash = "" + toTrashDate.Year + ("" + toTrashDate.Month).PadLeft(2, '0') + ("" + toTrashDate.Day).PadLeft(2, '0');
+            var toTrashAnomalies = "" + toTrashDate.Year + "-" + ("" + toTrashDate.Month).PadLeft(2, '0') + "-" + ("" + toTrashDate.Day).PadLeft(2, '0');
 
-            foreach (var i in Directory.EnumerateFiles(path).Where(row=>!row.ToLower().EndsWith(".xml") && !row.ToLower().EndsWith(".sessions")))
+            IEnumerable<string> files;
+            if (allowXML)
+                files = Directory.EnumerateFiles(path).Where(row => !row.ToLower().EndsWith(".sessions"));
+            else
+                files = Directory.EnumerateFiles(path).Where(row => !row.ToLower().EndsWith(".xml") && !row.ToLower().EndsWith(".sessions"));
+            foreach (var i in files)
             {
                 var p = Path.GetFileName(i).Split('.');
-                if(p[1].CompareTo(toTrash) <= 0)
+                if (p[1] == "xml" && p[0].Split('_').Last().Substring(0, 10).CompareTo(toTrashAnomalies) <= 0) // Anomalies?
+                {
+                    try
+                    {
+                        File.Delete(i);
+                        Console.WriteLine(i);
+                    }
+                    catch
+                    {
+                    }
+                }
+                else if (p[1].CompareTo(toTrash) <= 0)
                 {
                     try
                     {
