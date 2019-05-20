03d165354a01b976e51b7552eab8b591701cb343
Marc Bollhalder <marc.bollhalder@psi.ch>
2019-05-20 16:47:48 +0200

    Add projects needed for the failover clustering of the gw logger control service

---
+++ GW3/GWLoggerControlService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceProcess;
+
+namespace GWLoggerControlService
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            ServiceBase.Run(new ControlService());
+        }
+    }
+}
